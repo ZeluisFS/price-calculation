025ae753c4f8e8b71939b051248c8c2a43820860
Jose Ferreira dos Santos <jose-luis.santos.ext@siemens.com>
2023-03-24 16:51:54 +0000

    feat: Add price calculation logic

--- frontend/src/app/calculation/models/price-options.model.ts
+++ frontend/src/app/calculation/models/price-options.model.ts
@@ -2,3 +2,9 @@ export interface PriceOptions {
   code: string;
   value: string;
 }
+
+export enum  PriceCodes {
+  PriceWithoutVat = 'priceWithoutVat',
+  ValueAddedTax = 'valueAddedTax',
+  PriceIncludeVat = 'priceIncludeVat'
+}
--- frontend/src/app/calculation/pages/calculation/calculation.component.ts
+++ frontend/src/app/calculation/pages/calculation/calculation.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatRadioChange } from '@angular/material/radio';
 import { ONLY_NUMERIC_REGEX } from 'src/app/utils';
-import { CountryWithVatRates, PriceOptions } from '../../models';
+import { CountryWithVatRates, PriceCodes, PriceOptions } from '../../models';
 
 @Component({
   selector: 'app-calculation',
@@ -31,9 +31,9 @@ export class CalculationComponent {
   ];
 
   priceOptions: PriceOptions[] = [
-    {code: 'priceWithoutVat', value: 'Price without VAT'},
-    {code: 'valueAddedTax', value: 'Value-Added Tax'},
-    {code: 'priceIncludeVat', value: 'Price incl. VAT'}
+    {code: PriceCodes.PriceWithoutVat, value: 'Price without VAT'},
+    {code: PriceCodes.ValueAddedTax, value: 'Value-Added Tax'},
+    {code: PriceCodes.PriceIncludeVat, value: 'Price incl. VAT'}
   ];
 
   constructor(private fb: FormBuilder) { }
@@ -71,6 +71,45 @@ export class CalculationComponent {
     this.disableOtherPriceOptions(newPriceOptionCode);
   }
 
+  onPriceChange(event: Event, formControlName: string): void {
+    if (this.formGroup.get(formControlName)?.valid) {
+      const newValue = Number((event.target as HTMLInputElement).value);
+
+      if (newValue) {
+        const selectedVat = this.formGroup.get('vatRate')?.value;
+
+        switch (formControlName) {
+          case PriceCodes.PriceWithoutVat: {
+            const priceWithoutVat = newValue;
+            const valueAddedTax = this.roundTwoDecimalCases((selectedVat / 100) * priceWithoutVat);
+            const priceIncludeVat = this.roundTwoDecimalCases(priceWithoutVat + valueAddedTax);
+
+            this.formGroup.get(PriceCodes.ValueAddedTax)?.setValue(valueAddedTax);
+            this.formGroup.get(PriceCodes.PriceIncludeVat)?.setValue(priceIncludeVat);
+            break;
+          }
+          case PriceCodes.ValueAddedTax: {
+            const valueAddedTax = newValue;
+            const priceWithoutVat = this.roundTwoDecimalCases((valueAddedTax * 100) / selectedVat);
+            const priceIncludeVat = this.roundTwoDecimalCases(priceWithoutVat + valueAddedTax);
+
+            this.formGroup.get(PriceCodes.PriceWithoutVat)?.setValue(priceWithoutVat);
+            this.formGroup.get(PriceCodes.PriceIncludeVat)?.setValue(priceIncludeVat);
+            break;
+          }
+          case PriceCodes.PriceIncludeVat: {
+            const priceIncludeVat = newValue;
+            const priceWithoutVat = this.roundTwoDecimalCases(priceIncludeVat / ((selectedVat / 100) + 1));
+            const valueAddedTax = this.roundTwoDecimalCases(priceIncludeVat - priceWithoutVat);
+
+            this.formGroup.get(PriceCodes.PriceWithoutVat)?.setValue(priceWithoutVat);
+            this.formGroup.get(PriceCodes.ValueAddedTax)?.setValue(valueAddedTax);
+            break;
+          }
+        }
+      }
+    }
+  }
 
   private disableFormControl(controlName: string): void {
     this.formGroup.get(controlName)?.disable();
@@ -86,5 +125,9 @@ export class CalculationComponent {
         this.disableFormControl(option.code);
       }
     }
+  }
+
+  private roundTwoDecimalCases(number: number): number {
+    return Math.round((number + Number.EPSILON) * 100) / 100;
   }
 }
